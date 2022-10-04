b85de577bef23ee8a74fc138f3f498c132b91ed4
KetakiSarode <101462176+KetakiSarode@users.noreply.github.com>
2022-10-05 00:32:26 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,53 +1,136 @@
-
+<!DOCTYPE html>
 <html>
 <head>
-	<title>multi-user role-based-login-system</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <title>Admin</title>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+       <link rel="stylesheet" href="./assets/css/style.css"></link>
+  </head>
 </head>
-<body>
-      <div class="container d-flex justify-content-center align-items-center"
-      style="min-height: 100vh">
-      	<form class="border shadow p-3 rounded"
-      	      action="php/check-login.php" 
-      	      method="post" 
-      	      style="width: 450px;">
-      	      <h1 class="text-center p-3">LOGIN</h1>
-      	      <?php if (isset($_GET['error'])) { ?>
-      	      <div class="alert alert-danger" role="alert">
-				  <?=$_GET['error']?>
-			  </div>
-			  <?php } ?>
-		  <div class="mb-3">
-		    <label for="username" 
-		           class="form-label">User name</label>
-		    <input type="text" 
-		           class="form-control" 
-		           name="username" 
-		           id="username">
-		  </div>
-		  <div class="mb-3">
-		    <label for="password" 
-		           class="form-label">Password</label>
-		    <input type="password" 
-		           name="password" 
-		           class="form-control" 
-		           id="password">
-		  </div>
-		  <div class="mb-1">
-		    <label class="form-label">Select User Type:</label>
-		  </div>
-		  <select class="form-select mb-3"
-		          name="role" 
-		          aria-label="Default select example">
-                  <option selected value="user">Customer</option>
-			  <option value="admin">Admin</option>
-              <option value="admin">Organizer</option>
-		  </select>
-		 
-		  <button type="submit" 
-		          class="btn btn-primary">LOGIN</button>
-		</form>
-      </div>
-</body>
-</html>
+<body >
+    
+        <?php
+            include "./adminHeader.php";
+            include "./sidebar.php";
+           
+            include_once "./config/dbconnect.php";
+        ?>
 
+    <div id="main-content" class="container allContent-section py-4">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="card">
+                    <i class="fa fa-users  mb-2" style="font-size: 70px;"></i>
+                    <h4 style="color:white;">Total Users</h4>
+                    <h5 style="color:white;">
+                    <?php
+                        $sql="SELECT * from users where isAdmin=0";
+                        $result=$conn-> query($sql);
+                        $count=0;
+                        if ($result-> num_rows > 0){
+                            while ($row=$result-> fetch_assoc()) {
+                    
+                                $count=$count+1;
+                            }
+                        }
+                        echo $count;
+                    ?></h5>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <i class="fa fa-th-large mb-2" style="font-size: 70px;"></i>
+                    <h4 style="color:white;">Total Categories</h4>
+                    <h5 style="color:white;">
+                    <?php
+                       
+                       $sql="SELECT * from category";
+                       $result=$conn-> query($sql);
+                       $count=0;
+                       if ($result-> num_rows > 0){
+                           while ($row=$result-> fetch_assoc()) {
+                   
+                               $count=$count+1;
+                           }
+                       }
+                       echo $count;
+                   ?>
+                   </h5>
+                </div>
+            </div>
+            <div class="col-sm-3">
+            <div class="card">
+                    <i class="fa fa-th mb-2" style="font-size: 70px;"></i>
+                    <h4 style="color:white;">Total events</h4>
+                    <h5 style="color:white;">
+                    <?php
+                       
+                       $sql="SELECT * from product";
+                       $result=$conn-> query($sql);
+                       $count=0;
+                       if ($result-> num_rows > 0){
+                           while ($row=$result-> fetch_assoc()) {
+                   
+                               $count=$count+1;
+                           }
+                       }
+                       echo $count;
+                   ?>
+                   </h5>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <i class="fa fa-list mb-2" style="font-size: 70px;"></i>
+                    <h4 style="color:white;">Total orders</h4>
+                    <h5 style="color:white;">
+                    <?php
+                       
+                       $sql="SELECT * from orders";
+                       $result=$conn-> query($sql);
+                       $count=0;
+                       if ($result-> num_rows > 0){
+                           while ($row=$result-> fetch_assoc()) {
+                   
+                               $count=$count+1;
+                           }
+                       }
+                       echo $count;
+                   ?>
+                   </h5>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+       
+            
+        <?php
+            if (isset($_GET['category']) && $_GET['category'] == "success") {
+                echo '<script> alert("Category Successfully Added")</script>';
+            }else if (isset($_GET['category']) && $_GET['category'] == "error") {
+                echo '<script> alert("Adding Unsuccess")</script>';
+            }
+            if (isset($_GET['size']) && $_GET['size'] == "success") {
+                echo '<script> alert("Size Successfully Added")</script>';
+            }else if (isset($_GET['size']) && $_GET['size'] == "error") {
+                echo '<script> alert("Adding Unsuccess")</script>';
+            }
+            if (isset($_GET['variation']) && $_GET['variation'] == "success") {
+                echo '<script> alert("Variation Successfully Added")</script>';
+            }else if (isset($_GET['variation']) && $_GET['variation'] == "error") {
+                echo '<script> alert("Adding Unsuccess")</script>';
+            }
+        ?>
+
+
+    <script type="text/javascript" src="./assets/js/ajaxWork.js"></script>    
+    <script type="text/javascript" src="./assets/js/script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+</body>
+ 
+</html>
